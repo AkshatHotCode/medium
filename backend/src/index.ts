261c78efc67b56f82c3679186fbc00468c07b4d7
Akshat Rajvanshi <akshatrajvanshi14@gmail.com>
2024-11-26 02:40:24 +0000

    signup&signin created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,61 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { jwt, sign } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+    JWT_SECRET: string
+  }
+}>()
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  try{
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password
+      }
+    })
+
+    const token = await sign({id: user.id}, c.env.JWT_SECRET);
+
+    return c.json({
+      jwt: token
+    });
+  } catch(e) {
+    c.status(403);
+    return c.json({error:"error while signing up"});;
+  }
 })
 
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password
+    }
+  });
+
+  if(!user) {
+    c.status(403);
+    return c.json({error:"user nor found"});
+  }
+  
+  const jwt = await sign({id: user.id}, c.env.JWT_SECRET);
+  return c.json({jwt})
 })
 
 app.post('/api/v1/blog', (c) => {
@@ -18,12 +66,13 @@ app.put('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.get('/api/v1/blog', (c) => {
+app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
-
-
+app.get('api/v1/blog/bulk', (c) => {
+  return c.text("Hello hono!")
+})
 
 
 export default app
